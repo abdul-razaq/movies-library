57176270617ebb5f4d393a2bad471d3eaf1b31f8
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-10-07 20:08:53 +0100

    added webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = {
+	entry: ['babel-polyfill', './src/index.js'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index_build.js',
+	},
+	module: {
+		rules: [
+			{ test: /\.(js)$/, use: 'babel-loader' },
+			{ test: /\.svg$/, use: 'svg-inline-loader' },
+			{
+				test: /\.module\.s(a|c)ss$/,
+				use: [
+					isDevelopment ? 'style-loader' : MiniCSSExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { modules: true, sourceMap: isDevelopment },
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: isDevelopment,
+						},
+					},
+				],
+			},
+			{
+				test: /\.s(a|c)ss$/,
+				exclude: /.\module\.s(a|c)ss$/,
+				use: [
+					isDevelopment ? 'style-loader' : MiniCSSExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: isDevelopment,
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+		new webpack.EnvironmentPlugin({
+			NODE_ENV: 'production',
+		}),
+		new MiniCSSExtractPlugin({
+			filename: isDevelopment ? '[name].css' : '[name].[fullhash].css',
+			chunkFilename: isDevelopment ? '[id].css' : '[id].[fullhash].css',
+		}),
+	],
+	mode: isDevelopment ? 'development' : 'production',
+	resolve: {
+		extensions: ['.jsx', '.js', '.scss', '.sass'],
+	},
+};
