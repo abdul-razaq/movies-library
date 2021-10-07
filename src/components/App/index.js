c020b4ca5fdab0526044d1d18674627dae6d7008
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-10-07 23:46:25 +0100

    added functionality to toggle theme

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import { ThemeProvider } from '../../contexts/theme';
+
 import classes from './app.module';
 
 export default function App({}) {
+	const [theme, setTheme] = React.useState('light');
+
+	function toggleTheme() {
+		setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+	}
+
 	return (
-		<main className={classes.light}>
-			<h1>Welcome to Movies library</h1>
+		<main className={classes[theme]}>
+			<ThemeProvider
+				value={{
+					theme,
+					toggleTheme,
+				}}
+			>
+				<h1>Welcome to Movies library</h1>
+			</ThemeProvider>
 		</main>
 	);
 }
