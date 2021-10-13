84394c8c906671a0d3b3dc69eb16631240f66e55
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-10-13 11:28:59 +0100

    added action to fetch popular movies and reducer to handle popular movies

--- src/store/actions/movies/index.js
+++ src/store/actions/movies/index.js
@@ -2,20 +2,30 @@ import { getPopularMovies } from '../../../api/themoviedb';
 
 export const actionTypes = {
 	GET_POPULAR_MOVIES: 'GET_POPULAR_MOVIES',
+	SET_ERROR: 'SET_ERROR',
 };
 
-function getPopularMovies() {
-  return async function(dispatch, getState) {
-    try {
-      const popularMovies = await getPopularMovies();
-      dispatch({
-        type: actionTypes.GET_POPULAR_MOVIES,
-        payload: {
-          
-        },
-      });
-    } catch (error) {
-      
-    }
-  }
+function fetchPopularMovies() {
+	return async function (dispatch, getState) {
+		try {
+			const popularMovies = await getPopularMovies();
+			dispatch({
+				type: actionTypes.GET_POPULAR_MOVIES,
+				payload: {
+					movies: popularMovies,
+				},
+			});
+		} catch (error) {
+			dispatch({
+				type: actionTypes.SET_ERROR,
+				payload: {
+					error: error.message,
+				},
+			});
+		}
+	};
 }
+
+export default {
+	fetchPopularMovies,
+};
--- src/store/reducers/movies/index.js
+++ src/store/reducers/movies/index.js
@@ -1,6 +1,23 @@
+import { actionTypes } from '../../actions/movies';
 
-const initialState = {};
+const initialState = {
+	movies: [],
+	error: '',
+};
 
 export default function moviesReducer(state = initialState, action) {
-  return state;
+	switch (action.type) {
+		case actionTypes.GET_POPULAR_MOVIES:
+			return {
+				movies: action.payload.movies,
+				error: '',
+			};
+    case actionTypes.SET_ERROR:
+      return {
+        ...state,
+        error: action.payload.error
+      }
+		default:
+			return state;
+	}
 }
