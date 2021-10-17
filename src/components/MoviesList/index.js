cce996fd84a5a06503f179b286d2cad9bae0c40d
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-10-17 15:37:12 +0100

    added initial ui for shelf screen

--- src/components/MoviesList/index.js
+++ src/components/MoviesList/index.js
@@ -20,7 +20,9 @@ export default function MoviesList({
 	const { favoriteMovies, moviesWatching } = useSelector(state => state.shelf);
 
 	function handleToggleFavoritedMovie(movieId) {
-		console.log(`added or removed movie with ${movieId} from list of favorited movies.`)
+		console.log(
+			`added or removed movie with ${movieId} from list of favorited movies.`,
+		);
 	}
 
 	function handleToggleWatchingMovie(movieId) {
@@ -36,9 +38,11 @@ export default function MoviesList({
 					<h1>{category}</h1>
 					<h3>Movies</h3>
 				</div>
-				<p>
-					Page: {page} of {totalPages}
-				</p>
+				{page && totalPages && (
+					<p>
+						Page: {page} of {totalPages}
+					</p>
+				)}
 			</header>
 			<section className={classes.moviesList__content}>
 				{movies.map(({ poster_path, title, vote_average, id }) => (
@@ -54,16 +58,18 @@ export default function MoviesList({
 					/>
 				))}
 			</section>
-			<footer>
-				<SecondaryButton onClick={onGoBack}>
-					<FaArrowLeft />
-					<span>Back</span>
-				</SecondaryButton>
-				<SecondaryButton onClick={onGoToNextPage}>
-					<span>Page {nextPage}</span>
-					<FaArrowRight />
-				</SecondaryButton>
-			</footer>
+			{page && nextPage && (
+				<footer>
+					<SecondaryButton onClick={onGoBack}>
+						<FaArrowLeft />
+						<span>Back</span>
+					</SecondaryButton>
+					<SecondaryButton onClick={onGoToNextPage}>
+						<span>Page {nextPage}</span>
+						<FaArrowRight />
+					</SecondaryButton>
+				</footer>
+			)}
 		</article>
 	);
 }
@@ -71,9 +77,9 @@ export default function MoviesList({
 MoviesList.propTypes = {
 	category: PropTypes.string.isRequired,
 	movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-	page: PropTypes.number.isRequired,
-	nextPage: PropTypes.number.isRequired,
-	totalPages: PropTypes.number.isRequired,
-	onGoToNextPage: PropTypes.func.isRequired,
-	onGoBack: PropTypes.func.isRequired,
+	page: PropTypes.number,
+	nextPage: PropTypes.number,
+	totalPages: PropTypes.number,
+	onGoToNextPage: PropTypes.func,
+	onGoBack: PropTypes.func,
 };
--- src/screens/Shelf/index.js
+++ src/screens/Shelf/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
+import MoviesList from '../../components/MoviesList';
+import Center from '../../components/customs/Center';
+import NoData from '../../components/UI/NoData';
+
+import classes from './shelf.module';
+
 export default function ShelfScreen({}) {
-  const { type } = useParams();
-  return (
-    <h1>
-      SHELF SCREEN - Showing list of movies stored in { type } shelf.
-    </h1>
-  );
+	const { type } = useParams();
+
+	const { favoriteMovies, moviesWatching } = useSelector(state => state.shelf);
+
+	const shelf = { favorites: favoriteMovies, watching: moviesWatching };
+
+	return (
+		<section className={classes.shelf}>
+			{!shelf[type].length ? (
+				<Center>
+					<NoData
+						text={`You have not added any movie to your ${type} shelf. start by adding some.`}
+					/>
+				</Center>
+			) : (
+				<MoviesList category={type.replace('s', '')} movies={shelf[type]} />
+			)}
+		</section>
+	);
 }
