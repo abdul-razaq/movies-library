18773be9b67e7c1b5d9654296637446124b36fef
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-11-01 14:51:03 +0100

    refactored Dropdown component

--- src/components/UI/Dropdown/index.js
+++ src/components/UI/Dropdown/index.js
@@ -5,6 +5,47 @@ import ThemeContext from '../../../contexts/theme';
 
 import classes from './dropdown.module';
 
+function renderDropdown(
+	theme,
+	label,
+	showDropdown,
+	options,
+	handleFilterSelect,
+	setShowDropdown,
+) {
+	return (
+		<div className={classes.dropdown}>
+			{renderSelect(theme, label, showDropdown, setShowDropdown)}
+			{renderOptions(theme, options, showDropdown, handleFilterSelect)}
+		</div>
+	);
+}
+
+function renderSelect(theme, label, showDropdown, setShowDropdown) {
+	return (
+		<div
+			style={styles.select(theme)}
+			className={classes.select}
+			onClick={() => setShowDropdown(v => !v)}
+		>
+			<span>{label}</span>
+			<span>{showDropdown ? '\u25B2' : '\u25BC'}</span>
+		</div>
+	);
+}
+
+function renderOptions(theme, options, showDropdown, handleFilterSelect) {
+	return (
+		<ul className={classes.options} style={styles.options(theme, showDropdown)}>
+			{options.map(option => (
+				<li onClick={handleFilterSelect.bind(null, option)} key={option.label}>
+					{option.label}
+				</li>
+			))}
+		</ul>
+	);
+}
+
 export default function DropDown({ label, options, onSelect }) {
 	const [showDropdown, setShowDropdown] = React.useState(false);
 
@@ -15,42 +56,40 @@ export default function DropDown({ label, options, onSelect }) {
 		setShowDropdown(false);
 	}
 
-	return (
-		<div className={classes.dropdown}>
-			<div
-				style={{
-					backgroundColor:
-						theme === 'light'
-							? 'var(--color-light-secondary)'
-							: 'var(--color-dark-secondary)',
-				}}
-				className={classes.select}
-				onClick={() => setShowDropdown(v => !v)}
-			>
-				<span>{label}</span>
-				<span>{showDropdown ? '\u25B2' : '\u25BC'}</span>
-			</div>
-			<ul
-				className={classes.options}
-				style={{
-					backgroundColor:
-						theme === 'light'
-							? 'var(--color-light-secondary)'
-							: 'var(--color-dark-secondary)',
-          display: showDropdown ? 'block' : 'none',
-				}}
-			>
-				{options.map(option => (
-					<li
-						onClick={handleFilterSelect.bind(null, option)}
-						key={option.label}
-					>
-						{option.label}
-					</li>
-				))}
-			</ul>
-		</div>
+	return renderDropdown(
+		theme,
+		label,
+		showDropdown,
+		options,
+		handleFilterSelect,
+		setShowDropdown,
 	);
 }
 
-DropDown.propTypes = {};
+const styles = {
+	select(theme) {
+		return {
+			backgroundColor:
+				theme === 'light'
+					? 'var(--color-light-secondary)'
+					: 'var(--color-dark-secondary)',
+		};
+	},
+	options(theme, showDropdown) {
+		return {
+			backgroundColor:
+				theme === 'light'
+					? 'var(--color-light-secondary)'
+					: 'var(--color-dark-secondary)',
+			display: showDropdown ? 'block' : 'none',
+		};
+	},
+};
+
+DropDown.propTypes = {
+	label: PropTypes.string.isRequired,
+	options: PropTypes.arrayOf(
+		PropTypes.shape({ value: PropTypes.string, label: PropTypes.string }),
+	).isRequired,
+	onSelect: PropTypes.func.isRequired,
+};
