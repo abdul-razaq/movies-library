97751275591f7feb9b48dd7c87f4bf002a223302
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2021-11-03 02:51:31 +0100

    minor fix in movieDetails

--- src/screens/MovieDetails/index.js
+++ src/screens/MovieDetails/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import { BounceLoader } from 'react-spinners';
 import StarRatings from 'react-star-ratings';
 import ModalVideo from 'react-modal-video';
